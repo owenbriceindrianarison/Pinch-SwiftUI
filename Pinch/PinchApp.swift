6b134efd7c892ded52f80f0ddfb83782de3b5034
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-06-06 12:46:35 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Owen on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
